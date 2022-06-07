69b133d048e56b7b28978ca264166ede0950221e
George J Padayatti <george.padayatti@igrant.io>
2022-06-07 23:51:26 +0530

    Added method to generate DID document
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/ebsi_client/__init__.py
+++ src/ebsi_client/__init__.py
@@ -16,3 +16,23 @@ class EbsiClient:
     @property
     def eth(self) -> Ethereum:
         return self._eth
+
+    def generate_did_document(self) -> str:
+        return {
+            "@context": "https://www.w3.org/ns/did/v1",
+            "id": f"{self._ebsi_did.did}",
+            "verificationMethod": [
+                {
+                    "id": f"{self._ebsi_did.did}#keys-1",
+                    "type": "Secp256k1VerificationKey2018",
+                    "controller": f"{self._ebsi_did.did}",
+                    "publicKeyJwk": {key: value for key, value in self._eth.public_key_to_jwk().items() if key != "kid"}
+                }
+            ],
+            "authentication": [
+                f"{self._ebsi_did.did}#keys-1",
+            ],
+            "assertionMethod": [
+                f"{self._ebsi_did.did}#keys-1",
+            ],
+        }
