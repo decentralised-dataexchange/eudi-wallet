4f2564df0d2c5f81228b2af6c41b13709d4a66e4
George J Padayatti <george.padayatti@igrant.io>
2022-07-19 01:47:34 +0530

    Fix version prefix for did:ebsi v2 identifier
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/ebsi_did/__init__.py
+++ src/ebsi_did/__init__.py
@@ -1,17 +1,18 @@
 import secrets
 import base64
 from multibase import encode
+from jwcrypto.common import base64url_decode
 from ..ethereum import Ethereum
 
 
 class EbsiDid:
 
-    def __init__(self, did_version: int = 1):
+    def __init__(self, did_version: str = "v1"):
         self._did = None
         self._did_version = did_version
 
     def generate_did(self, eth: Ethereum = None):
-        if self.did_version == 2:
+        if self.did_version == "v2":
             self.generate_did_v2(eth=eth)
         else:
             self.generate_did_v1()
@@ -24,8 +25,8 @@ class EbsiDid:
 
     def generate_did_v2(self, eth: Ethereum):
         thumbprint = eth.jwk_thumbprint
-        subject_identifier = base64.urlsafe_b64decode(thumbprint + "==")
-        buffer = (1).to_bytes(2, 'big') + subject_identifier
+        subject_identifier = base64url_decode(thumbprint)
+        buffer = (2).to_bytes(2, 'big') + subject_identifier
         self._did = encode("base58btc", buffer).decode("utf-8")
         self._did = f"did:ebsi:{self.did}"
 
